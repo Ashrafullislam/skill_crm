a67d46b7413bd8b02caf145d82999be2d405a368
Ashrafullislam <ashrafull130@gmail.com>
2023-05-23 02:39:51 -0700

    update kpi-myfollowup table

--- src/Componants/Pages/Quarterly-KPI-Report/MyFollowup.js
+++ src/Componants/Pages/Quarterly-KPI-Report/MyFollowup.js
@@ -5,10 +5,10 @@ const MyFollowup = () => {
     <div className='w-full mx-auto mt-5'>
       {/* Quarterly KPI Report table start here  */}
        <div className="overflow-x-auto">
-          <table className="table w-full ">
+          <table className="table w-full  ">
             {/* head*/}
             <thead className=''>
-              <tr className=''>
+              <tr className='border'>
                 <th className='bg-blue-500 rounded-tl-sm rounded-bl-sm  font-extrabold '> Quarter</th>
                 <th className='bg-blue-500   font-extrabold '> Criteria</th>
                 <th className='bg-blue-500   font-extrabold '> New Call </th>
@@ -22,32 +22,32 @@ const MyFollowup = () => {
               {/* row 1 */}
                 <tr className='h-4 bg-gray-200 border-none '>
                 <th className='h-3 bg-gray-200 border-none'>  </th>
-                <td className='h-3 bg-gray-200 border'> Target</td>
-                <td className='h-3 bg-gray-200 border'> 0 </td>
-                <td className='h-3 bg-gray-200 border'> 0 </td>
-                <td className='h-3 bg-gray-200 border'> 0 </td>
-                <td className='h-3 bg-gray-200 border'> 0 </td>
+                <td className='h-3 bg-gray-200 border border-slate-300'> Target</td>
+                <td className='h-3 bg-gray-200 border border-slate-300'> 0 </td>
+                <td className='h-3 bg-gray-200 border border-slate-300'> 0 </td>
+                <td className='h-3 bg-gray-200 border border-slate-300'> 0 </td>
+                <td className='h-3 bg-gray-200 border border-slate-300'> 0 </td>
                 <td className='h-3 bg-gray-200 border-none'> </td>
               </tr >
               {/* row 2 */}
               <tr className="  bg-gray-200  font-semibold">
                 <th className='bg-gray-200 text-blue-500 border-none'>2022_Q2</th>
-                <td className='text-slate-900 border'> Archieve </td>
-                <td className='text-slate-900 border'> 106 </td>
-                <td className='text-slate-900 border'> 23 </td>
-                <td className='text-slate-900 border'> 4 </td>
-                <td className='text-slate-900 border'> 0 </td>
+                <td className='text-slate-900 border border-slate-300'> Archieve </td>
+                <td className='text-slate-900 border border-slate-300'> 106 </td>
+                <td className='text-slate-900 border border-slate-300'> 23 </td>
+                <td className='text-slate-900 border border-slate-300'> 4 </td>
+                <td className='text-slate-900 border border-slate-300'> 0 </td>
                 <td className='bg-gray-200 text-slate-900 border-none text-3xl'>0 % </td>
               </tr>
               {/* row 3 */}
-              <tr>
-                <th className=' bg-gray-200  '> </th>
-                <td className=' bg-gray-200 border '> Efficiency </td>
-                <td className=' bg-gray-200 border '> 0%</td>
-                <td className=' bg-gray-200 border '> 0% </td>
-                <td className=' bg-gray-200 border text-slate-900'> 0% </td>
-                <td className=' bg-gray-200 border text-slate-900'> 0%  </td>
-                <td className=' bg-gray-200  text-slate-900'>  </td>
+              <tr className='border'>
+                <th className=' bg-gray-200 border border-r-slate-300 '> </th>
+                <td className=' bg-gray-200 border border-slate-300'> Efficiency </td>
+                <td className=' bg-gray-200 border border-slate-300 '> 0%</td>
+                <td className=' bg-gray-200 border border-slate-300'> 0% </td>
+                <td className=' bg-gray-200 border border-slate-300 text-slate-900'> 0% </td>
+                <td className=' bg-gray-200 border border-slate-300 text-slate-900'> 0%  </td>
+                <td className=' bg-gray-200 border  text-slate-900'>  </td>
               </tr>
               
             </tbody>

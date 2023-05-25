f2f2588e6c07e116f94194ac53054e6f15063d11
Ashrafullislam <ashrafull130@gmail.com>
2023-05-24 21:04:26 -0700

    added my followup table with fake data

--- src/Componants/Pages/All_Leads/MyFollowUp.js
+++ src/Componants/Pages/All_Leads/MyFollowUp.js
@@ -1,184 +1,67 @@
 import React, { useEffect, useState } from 'react';
-import data from '../../../followupData.json' ;
 
 const MyFollowUp = () => {
 
-const FollowupData = 
-[
-   {
-      "_id": 1,
-      "company":"Larido Studio",
-      "country": " Poland",
-      "website": " larido.com",
-      "category": " Agency ",
-      "minor": "Tafannom  Mailat ",
-      "follower":" Tafannom Mailat",
-      "status": " Others ",
-      "possibility": " ",
-      "next-followup": " 03 Jun 2023",
-      "created-at": " 14 May 2023 04.22 PM",
-      "phone": " 014578477455" 
-    
-     },
-     {
-       "_id": 2,
-      "company":"Larido Studio",
-      "country": " Poland",
-      "website": " larido.com",
-      "category": " Agency ",
-      "minor": "Tafannom  Mailat ",
-      "follower":" Tafannom Mailat",
-      "status": " Others ",
-      "possibility": " ",
-      "next-followup": " 03 Jun 2023",
-      "created-at": " 14 May 2023 04.22 PM",
-      "phone": " 014578477455" 
-    
-     },
-     {
-       "_id": 3,
-      "company":"Larido Studio",
-      "country": " Poland",
-      "website": " larido.com",
-      "category": " Agency ",
-      "minor": "Tafannom  Mailat ",
-      "follower":" Tafannom Mailat",
-      "status": " Others ",
-      "possibility": " ",
-      "next-followup": " 03 Jun 2023",
-      "created-at": " 14 May 2023 04.22 PM",
-      "phone": " 014578477455" 
-    
-     },
-     {
-       "_id": 4,
-      "company":"Larido Studio",
-      "country": " Poland",
-      "website": " larido.com",
-      "category": " Agency ",
-      "minor": "Tafannom  Mailat ",
-      "follower":" Tafannom Mailat",
-      "status": " Others ",
-      "possibility": " ",
-      "next-followup": " 03 Jun 2023",
-      "created-at": " 14 May 2023 04.22 PM",
-      "phone": " 014578477455" 
-    
-     },
-     {
-       "_id": 5,
-      "company":"Larido Studio",
-      "country": " Poland",
-      "website": " larido.com",
-      "category": " Agency ",
-      "minor": "Tafannom  Mailat ",
-      "follower":" Tafannom Mailat",
-      "status": " Others ",
-      "possibility": " ",
-      "next-followup": " 03 Jun 2023",
-      "created-at": " 14 May 2023 04.22 PM",
-      "phone": " 014578477455" 
-    
-     },
-     {
-       "_id": 6,
-      "company":"Larido Studio",
-      "country": " Poland",
-      "website": " larido.com",
-      "category": " Agency ",
-      "minor": "Tafannom  Mailat ",
-      "follower":" Tafannom Mailat",
-      "status": " Others ",
-      "possibility": " ",
-      "next-followup": " 03 Jun 2023",
-      "created-at": " 14 May 2023 04.22 PM",
-      "phone": " 014578477455" 
-    
-     },
-     {
-       "_id": 7,
-      "company":"Larido Studio",
-      "country": " Poland",
-      "website": " larido.com",
-      "category": " Agency ",
-      "minor": "Tafannom  Mailat ",
-      "follower":" Tafannom Mailat",
-      "status": " Others ",
-      "possibility": " ",
-      "next-followup": " 03 Jun 2023",
-      "created-at": " 14 May 2023 04.22 PM",
-      "phone": " 014578477455" 
-    
-     }
-     
-   ]
 
+   const [userData,setUserData] = useState([]);
+   
    useEffect(()=> {
-      fetch(data)
-      .then(res => res.json())
-      .then(newData => {
-        console.log(newData)
-      })
-   },[])
+     const FetchData = async ()=> {
+      try{
+         const response = await fetch ('../../../followupData.json');
+         const data = await response.json()
+         setUserData(data)
+
+      }catch(error){
+        console.log("Failed to fetch data ",error)
+      }
+     }
+     FetchData()
+   },  [])
    
-   
+   console.log(userData)
     return (
          <div className='w-full '>
              <div className="overflow-x-auto">
-                <h3>  </h3>
-          <table className="table w-full  ">
+          <table className=" w-full  ">
             {/* head*/}
             <thead className=''>
               <tr className='border'>
-                <th className='bg-blue-500 rounded-tl-sm rounded-bl-sm  font-extrabold'> SL </th>
-                <th className='bg-blue-500  font-extrabold '> Company </th>
-                <th className='bg-blue-500  font-extrabold '> Country </th>
-                <th className='bg-blue-500  font-extrabold '> Website  </th>
-                <th className='bg-blue-500  font-extrabold '> Category </th>
-                <th className='bg-blue-500  font-extrabold '> Minor  </th>
-                <th className='bg-blue-500  font-extrabold '> Follower</th>
-                <th className='bg-blue-500  font-extrabold '> Status </th>
-                <th className='bg-blue-500  font-extrabold '> Possibility  </th>
-                <th className='bg-blue-500  font-extrabold '> Next Followup </th>
-                <th className='bg-blue-500  font-extrabold '> Created At </th>
-                <th className='bg-blue-500  font-extrabold '> Phone </th>
-                <th className='bg-blue-500 rounded-tr-sm rounded-br-sm font-extrabold '> Action </th>
+                <th className='bg-blue-500 rounded-tl-sm    py-3 rounded-bl-sm  font-extrabold'> SL </th>
+                <th className='bg-blue-500  font-extrabold  py-3'> Company </th>
+                <th className='bg-blue-500  font-extrabold  py-3'> Country </th>
+                <th className='bg-blue-500  font-extrabold  py-3'> Website  </th>
+                <th className='bg-blue-500  font-extrabold  py-3'> Category </th>
+                <th className='bg-blue-500  font-extrabold  py-3'> Minor  </th>
+                <th className='bg-blue-500  font-extrabold  py-3'> Follower</th>
+                <th className='bg-blue-500  font-extrabold  py-3'> Status </th>
+                <th className='bg-blue-500  font-extrabold  py-3 '> Possiblity  </th>
+                <th className='bg-blue-500  font-extrabold  py-3'> Next Followup </th>
+                <th className='bg-blue-500  font-extrabold  py-3'> Created At </th>
+                <th className='bg-blue-500  font-extrabold  py-3'> Phone </th>
+                <th className='bg-blue-500 rounded-tr-sm    py-3 rounded-br-sm font-extrabold '> Action </th>
               </tr>
             </thead>
             <tbody className='border font-semibold body-col  '>
               {/* row 1 */}
-                <tr className='h-4 bg-gray-200 border border-slate-300 '>
-                <th className='h-3 bg-gray-200 border border-slate-300'> 1 </th>
-                <td className='h-3 bg-gray-200 border border-slate-300'> 2</td>
-                <td className='h-3 bg-gray-200 border border-slate-300'> 3 </td>
-                <td className='h-3 bg-gray-200 border border-slate-300'> 4 </td>
-                <td className='h-3 bg-gray-200 border border-slate-300'> 5 </td>
-                <td className='h-3 bg-gray-200 border border-slate-300'> 6 </td>
-                <td className='h-3 bg-gray-200 border border-slate-300'> 7 </td>
-                <td className='h-3 bg-gray-200 border border-slate-300'> 8 </td>
-                <td className='h-3 bg-gray-200 border border-slate-300'> 9 </td>
-                <td className='h-3 bg-gray-200 border border-slate-300'> 10 </td>
-                <td className='h-3 bg-gray-200 border border-slate-300'> 11 </td>
-                <td className='h-3 bg-gray-200 border border-slate-300'> 12 </td>
-                <td className='h-3 bg-gray-200 border border-slate-300'> 13</td>
-              </tr >
-              {/* row 2 */}
-              <tr className=" bg-gray-200  font-semibold">
-                <th className=' text-blue-500 border-slate-300'>2</th>
-                <td className=' text-slate-900 border border-slate-300'> 2</td>
-                <td className=' text-slate-900 border border-slate-300'> 3 </td>
-                <td className=' text-slate-900 border border-slate-300'> 4 </td>
-                <td className=' text-slate-900 border border-slate-300'> 5 </td>
-                <td className=' text-slate-900 border border-slate-300'> 6 </td>
-                <td className=' text-slate-900 border border-slate-300'> 7 </td>
-                <td className=' text-slate-900 border border-slate-300'>8 </td>
-                <td className=' text-slate-900 border border-slate-300'>9 </td>
-                <td className=' text-slate-900 border border-slate-300'>10 </td>
-                <td className=' text-slate-900 border border-slate-300'>11 </td>
-                <td className=' text-slate-900 border border-slate-300'>12 </td>
-                <td className=' text-slate-900 border border-slate-300'>13 </td>
+                {userData.map((userInfo,index) => (
+                <tr key={index} className='h-4 bg-gray-200 border border-slate-300 '>
+                <th className='h-3 bg-gray-200 border border-slate-300 py-0 text-center px-0  m-1'> {index+1}   </th>
+                <td className='h-3 bg-gray-200 border border-slate-300 py-0 text-center px-0  m-1'> {userInfo.company} </td>
+                <td className='h-3 bg-gray-200 border border-slate-300 py-0 text-center px-0  m-1'> {userInfo.country} </td>
+                <td className='h-3 bg-gray-200 border border-slate-300 py-0 text-center px-0  m-1'> {userInfo.website} </td>
+                <td className='h-3 bg-gray-200 border border-slate-300 py-0 text-center px-0  m-1'> {userInfo.category} </td>
+                <td className='h-3 bg-gray-200 border border-slate-300 py-0 text-center px-0  m-1'> {userInfo.minor} </td>
+                <td className='h-3 bg-gray-200 border border-slate-300 py-0 text-center px-0  m-1'> {userInfo.follower} </td>
+                <td className='h-3 bg-gray-200 border border-slate-300 py-0 text-center px-0  m-1'> {userInfo.status} </td>
+                <td className='h-3 bg-gray-200 border border-slate-300 py-0 text-center px-0  m-1'> {userInfo.possibility} </td>
+                <td className='h-3 bg-gray-200 border border-slate-300 py-0 text-center px-0  m-1'> {userInfo.next_followup} </td>
+                <td className='h-3 bg-gray-200 border border-slate-300 py-0 text-center px-0  m-1 w-24 '> {userInfo.created_at} </td>
+                <td className='h-3 bg-gray-200 border border-slate-300 py-0 text-center px-0  m-1'> {userInfo.phone} </td>
+                <td className='h-3 bg-gray-200 border border-slate-300 py-0 text-center px-0  m-1'> 13</td>
                 </tr>
-             
+                ))}
+                 
               
             </tbody>
           </table>
